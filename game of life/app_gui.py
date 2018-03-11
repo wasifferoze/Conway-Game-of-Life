701ed74f82ac4018d3676a5b2fbf7b9175e2f384
wasifferoze <wasif_feroze@yahoo.com>
2018-03-11 19:03:59 +0800

    combox done

--- game of life/app_gui.py
+++ game of life/app_gui.py
@@ -4,7 +4,10 @@ from  tkinter import ttk
 
 
 class GUI(tk.Frame):
-    """for graphical interface"""
+    """for graphical interface
+
+    -all user graphical interface are here
+    """
     WIDTH = 803
     HEIGHT = 506
 
@@ -14,33 +17,49 @@ class GUI(tk.Frame):
         self.canvas = tk.Canvas(self, width=self.WIDTH, height=self.HEIGHT)
         self.canvas.pack(side=tk.TOP)
         self.control_area = tk.Frame(self)
+        self.control_area.config(bg="#808080")
         self.control_area.pack(side=tk.BOTTOM, fill=tk.X)
         self.create_widgets()
 
     def create_widgets(self):
-        self.start = tk.Button(self.control_area, text="Start", command=self.start_game)
-        self.start.pack(side=tk.RIGHT)
-        self.stop = tk.Button(self.control_area, text="Stop", fg="red", command=self.stop_game)
-        self.stop.pack(side=tk.RIGHT)
+        """all widget code is here"""
         tkvar = tk.StringVar(win)
         # Dictionary with options
         choices = ('Clear', 'Small Glider', 'Glider', 'Exploder', '10 Cell Row', 'Light Weight Spaceship', 'Tumbler',
                    'Gosper Glider Gu')
-        # tkvar.set('Clear')  # set the default option
-        # popupMenu = tk.OptionMenu(self.control_area, tkvar, *choices)
-        # popupMenu.pack(side=tk.LEFT)
-        self.combo_input = ttk.Combobox(self.control_area, values=choices)
+        self.combo_input = ttk.Combobox(self.control_area, width=25, values=choices, state='readonly')
         self.combo_input.pack(side=tk.LEFT)
         self.combo_input.current(0)
+        self.combo_input.bind("<<ComboboxSelected>>", self.combo_callback)
+
+        self.next = tk.Button(self.control_area, text="Next", command=self.next_generation)
+        self.next.pack(side=tk.LEFT)
+        self.start = tk.Button(self.control_area, text="Start", command=self.start_game)
+        self.start.pack(side=tk.LEFT)
+
+        self.stop = tk.Button(self.control_area, text="Stop", fg="red", command=self.stop_game)
+        self.stop.pack(side=tk.LEFT)
+
+
+    def combo_callback(self, eventobj):
+        """this method get combobox events"""
+        print(self.combo_input.get())   # print name
+        print(self.combo_input.current())   # print index
 
     def start_game(self):
+        """game start flag on and off code here"""
         print("hi there, game started!")
 
     def stop_game(self):
         print("hi there! game stopped")
 
+    def next_generation(self):
+        print("next generation")
+
 
 win = tk.Tk()
+win.title("Conway's Game of Life")
+win.resizable(0,0)
 # win.geometry('+%d+%d' % (200, 200))
 app_gui = GUI(master=win)
 win.mainloop()
